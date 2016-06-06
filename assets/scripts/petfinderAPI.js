30eba88b7d8e98a8df7da7943527d0d42be031ff
susandaiute <susandaiute@gmail.com>
2016-06-06 15:43:12 -0400

    Add reference to Petfinder API access key, secret

--- assets/scripts/petfinderAPI.js
+++ assets/scripts/petfinderAPI.js
@@ -1,20 +1,16 @@
 'use strict';
 
+KEY=Rails.application.secrets.PETFINDER_KEY;
+SECRET=Rails.application.secrets.PETFINDER_SECRET;
 
 const getPhotos = (getPhotosSuccess, getPhotosFailure, searchTerm) => {
   $.ajax({
     method: 'GET',
     url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=479994a81bb05629ca3986e2eb43abe6&tags=landscape,' + searchTerm  + '&per_page=1&sort=interestingness-desc&format=json&nojsoncallback=1',
-  }).done(getPhotosSuccess)
-    .fail(getPhotosFailure);
+  }).done(success)
+    .fail(failure);
 };
 
-// Import module
-var petfinder = require('petfinder')('api_key','api_secret');
 
-// Get list of breeds
-petfinder.getBreedList('cat', function(err, breeds) {
-  console.log(breeds);
-});
 
 module.exports = true;
