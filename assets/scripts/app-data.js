ea17ca9e06e52111094e7e228eb2ed7e6488974f
susandaiute <susandaiute@gmail.com>
2016-06-22 00:03:54 -0400

    Point front end to heroku

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const app = {
-  api: 'http://localhost:3000',
+  api: 'https://aqueous-tor-31019.herokuapp.com',
 };
 
-//' https://aqueous-tor-31019.herokuapp.com',
+
 
 module.exports = app;
